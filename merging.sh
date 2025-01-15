5ef098c1fe6d77ec80fc5c51979771455fdeec32
adicere <rutkovskaya.ea@gmail.com>
2025-01-15 21:11:53 +0300

    multivariant sites splitting is added

--- merging.sh
+++ merging.sh
@@ -8,22 +8,40 @@ set -euo pipefail
 list=$1 # txt file with paths to vcf files
 o_dir=$2 # output directory for a merged file 
 
+temp="${o_dir}temp"
+res_dir="${o_dir}results"
+mkdir $temp $res_dir
+
+#split multiallelic sites
+files=$( cat "$list")
+for f in $files 
+do
+filename=$(basename $f)
+basename=${filename%%.*}
+bcftools norm -m -snps --multi-overlaps . -a --atom-overlaps . $f -Oz -o $temp/"$basename"_splitted.vcf.gz
+tabix $temp/"$basename"_splitted.vcf.gz
+done
+
+#save paths to splitted files
+find "$temp" -type f -name '*_splitted.vcf.gz' > $temp/splitted_files.txt 
+
 #merge all vcf files 
-bcftools merge --file-list "$list" -0 -m snps -Oz -o "$o_dir"/merged.vcf.gz
-tabix "$o_dir"/merged.vcf.gz
+bcftools merge --file-list $temp/"splitted_files.txt" -0 -m snps -Oz -o $res_dir/merged.vcf.gz 
+tabix $res_dir/merged.vcf.gz
+rm -rf "$temp"
 
 #calculating average VAF for each sample - HANDLE PATHS
-python /home/katerine/Desktop/VDKN/CAH_haplotypes/vaf.py "$o_dir"/merged.vcf.gz '/home/katerine/Desktop/VDKN/russian_field_of_experiments/'
+python /home/katerine/Desktop/VDKN/CAH_haplotypes/vaf.py $res_dir/merged.vcf.gz '/home/katerine/Desktop/VDKN/russian_field_of_experiments/results/'
 
 #removing unnecessary FORMAT fields except for GT
-zcat "$o_dir"/merged.vcf.gz | bcftools annotate -x ^FORMAT/GT -Oz -o "$o_dir"/genotype_merged.vcf.gz 
-tabix "$o_dir"/genotype_merged.vcf.gz 
+zcat $res_dir/merged.vcf.gz | bcftools annotate -x ^FORMAT/GT -Oz -o $res_dir/genotype_merged.vcf.gz 
+tabix $res_dir/genotype_merged.vcf.gz 
 
 #separate genotypes to chromosomes 
-python /home/katerine/Desktop/VDKN/CAH_haplotypes/vcf_processing.py "$o_dir"/genotype_merged.vcf.gz \
+python /home/katerine/Desktop/VDKN/CAH_haplotypes/vcf_processing.py $res_dir/genotype_merged.vcf.gz \
 /home/katerine/Desktop/VDKN/russian_field_of_experiments/scripts/variants.xlsx \
-/home/katerine/Desktop/VDKN/russian_field_of_experiments/VAF.json \
-/home/katerine/Desktop/VDKN/russian_field_of_experiments/
+/home/katerine/Desktop/VDKN/russian_field_of_experiments/results/VAF.json \
+/home/katerine/Desktop/VDKN/russian_field_of_experiments/results/ 
 
 
 
