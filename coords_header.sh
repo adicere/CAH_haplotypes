fd93e203a3da938e8bb1b0edd437cce79ab49735
adicere <rutkovskaya.ea@gmail.com>
2025-02-05 18:25:43 +0300

    latest fixed preprocessing of samples

--- coords_header.sh
+++ coords_header.sh
@@ -14,10 +14,11 @@ do
     id=`echo $f | grep -o -E '[0-9]{8}'` #extracts the first 8 numbers of each provided file in a list 
     bcftools view -h "$f" > header.txt 
     sed -i "s/pool/$id/" header.txt 
-    #bcftools view -h ../vcfs/770924920801.haplotype.caller.phased.vcf | sed "s/pool/77092492/" | grep -v -E '^##FORMAT=<ID=(GT|AD|DP|GQ|PL|PS),' > header.txt
-    python Anton/process_vcf.py $f temp.txt
+    python /home/katerine/Desktop/VDKN/russian_field_of_experiments/scripts/Anton/process_vcf.py $f temp.txt
     cat header.txt temp.txt | bgzip > "$2${id}.haplotype.caller.phased.coord.rehead.vcf.gz" && tabix "$2${id}.haplotype.caller.phased.coord.rehead.vcf.gz"
     rm header.txt temp.txt 
+    # bcftools view -i 'QUAL>10000' "$2${id}.haplotype.caller.phased.coord.rehead.vcf.gz" -Oz -o "$2${id}.haplotype.caller.phased.coord.rehead.filtered.vcf.gz"
+    # tabix "$2${id}.haplotype.caller.phased.coord.rehead.filtered.vcf.gz"
 done
 
-find "$(pwd)" -type f -wholename "$dir_out/*.haplotype.caller.phased.coord.rehead.vcf.gz" > gzip_paths.txt #creates a list for new .vcf.gz for the next step
+find "$dir_out" -maxdepth 1 -type f -name "*.haplotype.caller.phased.coord.rehead.vcf.gz" > "$dir_out"/gzip_paths.txt #creates a list for new .vcf.gz for the next step
