f26d583e015b0e43ce95a85602821b4f92e58fb7
Ekaterina Rutkovskaya <rutkovskaya.ea@gmail.com>
2025-06-22 01:06:42 +0300

    modified script with argument parser added

--- coords_header.sh
+++ coords_header.sh
@@ -6,24 +6,31 @@ Script combines recalibration of variant coordinates and add changes to the head
 
 set -euo pipefail
 
-files=$1 # .txt list with paths to the samples
-dir_out=$2 # output directory for processed vcf's
+while getopts 'h:d:s:o:' flag; do
+    case "${flag}" in 
+        h) 
+            hc_file=${OPTARG} ;;
+        d)
+            dv_file=${OPTARG} ;;
+        s)
+            sample=${OPTARG} ;;
+        o)
+            output_dir=${OPTARG} ;;
+    esac
+done
 
-while IFS=$'\t' read -r hc dv; do
-    id=`echo $hc | grep -o -E '[0-9]{8}'`
-    bcftools view -h "$hc" > hc_header.txt
-    sed -i "s/pool/$id/" hc_header.txt
-    python /home/katerine/Desktop/VDKN/russian_field_of_experiments/scripts/Anton/process_vcf.py $hc hc_temp.txt
-    cat hc_header.txt hc_temp.txt | bgzip > "$2${id}.haplotypecaller.phased.coord.rehead.vcf.gz" && tabix "$2${id}.haplotypecaller.phased.coord.rehead.vcf.gz"
+current_dir="$(dirname "$(realpath "$0")")"
 
-    bcftools view -h "$dv" > dv_header.txt  
-    sed -i "s/pool/$id/" dv_header.txt 
-    python /home/katerine/Desktop/VDKN/russian_field_of_experiments/scripts/Anton/process_vcf.py $dv dv_temp.txt
-    cat dv_header.txt dv_temp.txt | bgzip > "$2${id}.deepvariant.coord.rehead.vcf.gz" && tabix "$2${id}.deepvariant.coord.rehead.vcf.gz"
+#modifying the HaplotypeCaller VCF file
+bcftools view -h "${hc_file}" > hc_header.txt
+sed -i "s/pool/$sample/" hc_header.txt
+python "$current_dir/modify_vcf.py" "$hc_file" hc_temp.txt
+cat hc_header.txt hc_temp.txt | bgzip > "$output_dir/${sample}.hc.rehead.vcf.gz" && tabix "$output_dir/${sample}.hc.rehead.vcf.gz"
 
-    rm hc_header.txt hc_temp.txt dv_header.txt dv_temp.txt 
-done < "$files"
+#modifying the DeepVariant VCF file
+bcftools view -h "${dv_file}" > dv_header.txt
+sed -i "s/pool/$sample/" dv_header.txt
+python "$current_dir/modify_vcf.py" "$dv_file" dv_temp.txt
+cat dv_header.txt dv_temp.txt | bgzip > "$output_dir/${sample}.dv.rehead.vcf.gz" && tabix "$output_dir/${sample}.dv.rehead.vcf.gz"
 
-#creates a list for new .vcf.gz for the next step
-find "$dir_out" -maxdepth 1 -type f -name "*.haplotypecaller.phased.coord.rehead.vcf.gz" > "$dir_out"/hc_gz_paths.txt
-find "$dir_out" -maxdepth 1 -type f -name "*.deepvariant.coord.rehead.vcf.gz" > "$dir_out"/dv_gz_paths.txt  
+rm hc_header.txt hc_temp.txt dv_header.txt dv_temp.txt 
