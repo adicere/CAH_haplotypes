752ccc68298cbc53c649acea5e6f499016498da7
adicere <rutkovskaya.ea@gmail.com>
2025-10-14 14:56:59 +0000

    updating annotation with new files

--- update_ann.py
+++ update_ann.py
@@ -6,9 +6,9 @@ import argparse
 import subprocess
 import glob
 
-bcftools_path = '/home/rutkovskaya.ea/miniforge3/envs/haplotypes/bin/bcftools'
-reference = '/home/rutkovskaya.ea/haplotypes/CYP21A2-amp_hg38.fasta'
-modificator='/home/rutkovskaya.ea/haplotypes/CAH_haplotypes/reheader.sh'
+bcftools_path = '/primary/home/rutkovskaya.ea/miniforge3/envs/haplotypes/bin/bcftools'
+reference = '/primary/home/rutkovskaya.ea/haplotypes/CYP21A2-amp_hg38.fasta'
+modificator='/primary/home/rutkovskaya.ea/haplotypes/CAH_haplotypes/modificator.sh'
 
 
 updater = argparse.ArgumentParser(
@@ -22,7 +22,7 @@ Results of naive calling would be stored at $OUTPUT_DIR/mpileup, all corrected b
 )
 
 updater.add_argument('ann_file', type=str, help='provide a path to the table with filepaths of processed amplicons')
-updater.add_argument('-o', '--output', type=str, help='provide a path to the output directory to store the results', required = True, metavar = 'OUTPUT_DIR')
+updater.add_argument('-o', '--output', type=str, help='provide an ABSOLUTE path to the output directory to store the results', required = True, metavar = 'OUTPUT_DIR')
 updater.add_argument('-e', '--exclude', type=str, help='provide a txt file with samples that need to be excluded', metavar = 'FILE')
 
 args=updater.parse_args()
@@ -31,9 +31,11 @@ amplicon_ann = pd.read_csv(args.ann_file, sep='\t', index_col=0)
 # amplicon_ann = pd.read_excel(args.ann_file, index_col=0)
 # short=amplicon_ann.copy().iloc[:5, :]
 mpileup_dir=os.path.join(args.output, 'mpileup')  
-reheaded_dir = os.path.join(args.output, 'reheaded_vcfs')  
+# reheaded_dir = os.path.join(args.output, 'reheaded_vcfs')
+# splitted_vcf = os.path.join(args.output, 'splitted_vcfs')  
 os.makedirs(mpileup_dir, exist_ok=True)   
-os.makedirs(reheaded_dir, exist_ok=True)   
+# os.makedirs(reheaded_dir, exist_ok=True) 
+# os.makedirs(splitted_vcf, exist_ok=True)  
 
 def exclude_samples(ann_file, sample_list):
     samples=sample_list.read().split('\n')
@@ -46,38 +48,43 @@ def exclude_samples(ann_file, sample_list):
 def modify_vcf(ann_file):
 
     """
-    Function recalibrates the coordinates of amplicon VCF files of corresponding caller,
+    Function splits multiallelic sites, and also rewrites the coordinates of amplicon in VCF files of corresponding caller,
     changes pool to sample_id in the header and writes paths to new files into
     original annotation
     """
 
-    ann_file['hc_modified'] = ''
-    ann_file['dv_modified'] = ''
+    methods = {
+        'hc': 'hc_vcf',
+        'dv': 'deepvariant',
+        'mp': 'mpileup'
+    }
+
+    for m in methods:
+        ann_file[f'{m}_modified'] = ''
+        ann_file[f'{m}_splitted'] = ''
+     
     for idx, row in ann_file.iterrows():
         sample = str(row['orig_sample'])[:8]
-        hc_vcf = row['hc_vcf']
-        dv_vcf = row['deepvariant']
-        subprocess.run(['bash', modificator, 
-                        '-f', hc_vcf, 
-                        '-t', 'hc', 
-                        '-s', sample, 
-                        '-o', reheaded_dir])
-        subprocess.run(['bash', modificator, 
-                        '-f', dv_vcf, 
-                        '-t', 'dv', 
-                        '-s', sample, 
-                        '-o', reheaded_dir])
-        ann_file.at[idx, 'hc_modified'] = f'{reheaded_dir}/{sample}.hc.rehead.vcf.gz'
-        ann_file.at[idx, 'dv_modified'] = f'{reheaded_dir}/{sample}.dv.rehead.vcf.gz'
-    
+
+        for m, column in methods.items():
+            subprocess.run(['bash', modificator, 
+                            '-f', row[column], 
+                            '-t', m, 
+                            '-s', sample, 
+                            '-o', args.output])
+
+            ann_file.loc[idx, [f'{m}_modified', f'{m}_splitted']] = [
+                f'{args.output}/reheaded_vcfs/{sample}.{m}.rehead.vcf.gz',
+                f'{args.output}/splitted_vcfs/{sample}_splitted.{m}.vcf.gz'
+            ]
+
     return ann_file
 
 
 def naive_calling(ann_file):
 
     """
-    Function performs naive calling on bam files using bcftools mpileup+call, then
-    modifies resulted VCF like in modify_vcf()
+    Function performs naive calling on bam files using bcftools mpileup+call
     """
 
     ann_file['mpileup'] = ''
@@ -89,7 +96,7 @@ def naive_calling(ann_file):
                                     'mpileup',
                                     '-f', reference, 
                                     '-a', 'FORMAT/AD,FORMAT/DP', 
-                                    '--no-BAQ', '-d', '3000',
+                                    '-d', '3000',
                                     '-Ou', bam], stdout=subprocess.PIPE)
         calling =  subprocess.Popen([bcftools_path, 'call',
                                      '-mv', '-Oz', 
@@ -98,22 +105,16 @@ def naive_calling(ann_file):
         calling.wait()           
         mpileup.wait() 
         ann_file.at[idx, 'mpileup'] = filename
-        subprocess.run(['bash', modificator, 
-                        '-f', filename, 
-                        '-t', 'mpileup', 
-                        '-s', sample, 
-                        '-o', reheaded_dir])
-
-        ann_file.at[idx, 'mpileup_modified'] = f'{reheaded_dir}/{sample}.mpileup.rehead.vcf.gz'
     
-    return res
-   
+    return ann_file
 
+   
 if args.exclude is not None:
     to_exclude = open(args.exclude) 
     filtered_list=exclude_samples(amplicon_ann, to_exclude)
 else:
-    res = modify_vcf(amplicon_ann)
-    res=naive_calling(amplicon_ann)
+    naive_calling(amplicon_ann)
+    res=modify_vcf(amplicon_ann)
 
-res.to_excel(args.output+'modified_annotation.xlsx')
+output_path = os.path.join(args.output, 'modified_annotation.xlsx')
+res.to_excel(output_path)
